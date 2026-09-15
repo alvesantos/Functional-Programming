db693b505bb0fe3124c220489272825829208c57
alvesantos <alvesantos.gh@gmail.com>
2026-09-15 15:48:53 -0300

    solve palindrome

--- Decorators/LRU Cache/main.py
+++ Decorators/LRU Cache/main.py
@@ -1,6 +1,11 @@
-# ?
+from functools import lru_cache
 
-
-# ?
+@lru_cache()
 def is_palindrome(word: str) -> bool:
-    pass
+    if len(word) == 0 or len(word) == 1:
+        return True
+
+    if word[0] != word[len(word) - 1]:
+        return False
+
+    return is_palindrome(word[1:-1])
