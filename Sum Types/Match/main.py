e31b534cef2696caa3cb5e98617294a36788f7c1
alvesantos <alvesantos.gh@gmail.com>
2026-09-20 13:09:23 -0300

    solve match exercise

--- Sum Types/Match/main.py
+++ Sum Types/Match/main.py
@@ -6,14 +6,12 @@ class DocFormat(Enum):
     HTML = 4
 
 def convert_format(content: str, from_format: DocFormat, to_format: DocFormat | None) -> str:
-    if from_format == DocFormat.MD and to_format == DocFormat.HTML:
-        return "MD para HTML"
-
-    elif from_format == DocFormat.TXT and to_format == DocFormat.PDF:
-        return "TXT para PDF"
-
-    elif from_format == DocFormat.HTML and to_format == DocFormat.MD:
-        return "HTML para MD"
-    else:
-        raise Exception("invalid type")
-
+    match (from_format, to_format):
+        case (DocFormat.MD, DocFormat.HTML):
+            return content.replace("# ", "<h1>") + "</h1>"
+        case (DocFormat.TXT, DocFormat.PDF):
+            return f"[PDF] {content} [PDF]"
+        case (DocFormat.HTML, DocFormat.MD):
+            return content.replace("<h1>", "# ").replace("</h1>", "")
+        case _:
+            raise Exception("invalid type")
