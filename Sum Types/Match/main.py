137dfdce23a11c4944a49ced0c07fa7c89018e70
alvesantos <alvesantos.gh@gmail.com>
2026-09-20 12:56:48 -0300

    create a initial validation to the task

--- Sum Types/Match/main.py
+++ Sum Types/Match/main.py
@@ -1,17 +1,19 @@
 from enum import Enum
-
-
 class DocFormat(Enum):
     PDF = 1
     TXT = 2
     MD = 3
     HTML = 4
 
+def convert_format(content: str, from_format: DocFormat, to_format: DocFormat | None) -> str:
+    if from_format == DocFormat.MD and to_format == DocFormat.HTML:
+        return "MD para HTML"
 
-# Don't touch above this line
+    elif from_format == DocFormat.TXT and to_format == DocFormat.PDF:
+        return "TXT para PDF"
 
+    elif from_format == DocFormat.HTML and to_format == DocFormat.MD:
+        return "HTML para MD"
+    else:
+        raise Exception("invalid type")
 
-def convert_format(
-    content: str, from_format: DocFormat, to_format: DocFormat | None
-) -> str:
-    pass
